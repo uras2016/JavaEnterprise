47051f780bf98befd4671c04f2fd86feb30dd814
uras <uras@i.ua>
2016-06-23 19:54:28 +0300

    module3

--- src/module3_2/SquareSumRealization.java
+++ src/module3_2/SquareSumRealization.java
@@ -1,47 +1,86 @@
 package module3_2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class SquareSumRealization implements SquareSum {
+
+
+
+
     @Override
     public long getSquareSum(int[] values, int numberOfThreads) {
 
 
+        ExecutorService service = Executors.newFixedThreadPool(numberOfThreads);
 
 
+        int part = values.length/numberOfThreads;
 
-
-
-        return 0;
-    }
-
-
-
-
-
-
-
-
-
-
-
-    public long getSquareSumFirst(int[] value){
-        int halfLength = value.length/2;
-        int squareSum1 = 0;
-
-        for (int i = 0; i < halfLength; i++) {
-            int temp = value[i]*value[i];
-            squareSum1 +=temp;
+        List<Long> runnables = Collections.synchronizedList(new ArrayList<>());
+        Phaser phaser = new Phaser(1);
+        int start = 0;
+        int end = part;
+        for (int i = 0; i < numberOfThreads; i++) {
+            if (i + 1 == numberOfThreads) end = values.length;
+            Future<Long> f = service.submit(new MyCallable(values, start, end, runnables, phaser));
+            phaser.register();
+            start= start+part;
+            end = end+part;
         }
-        return squareSum1;
-    }
 
-    public long getSquareSumSecond(int[] value){
-        int halfLength = value.length/2;
-        int squareSum1 = 0;
 
-        for (int i = halfLength; i < value.length; i++) {
-            int temp = value[i]*value[i];
-            squareSum1 +=temp;
+        phaser.arriveAndAwaitAdvance();
+        phaser.arriveAndDeregister();
+        service.shutdown();
+
+
+        long finalResult =0;
+        for (Long number : runnables) {
+            finalResult+=number;
         }
-        return squareSum1;
+        return finalResult;
     }
+
+
+
+
+
+    public class MyCallable implements Callable<Long>{
+        int[] values;
+        int start;
+        int end;
+        List<Long> result;
+        Phaser ph;
+
+        public MyCallable(int[] values, int start, int end, List<Long> result, Phaser ph) {
+            this.values = values;
+            this.start = start;
+            this.end = end;
+            this.result = result;
+            this.ph = ph;
+        }
+
+        @Override
+        public Long call() {
+
+            Long sum = 0L;
+
+            for (int i = start; i < end; i++) {
+                sum += values[i] * values[i];
+            }
+//            System.out.println(sum);
+
+            result.add(sum);
+            ph.arriveAndDeregister();
+
+            return sum;
+        }
+    }
+
+
+
+
 }
--- test/module3_2/SquareSumRealizationTest.java
+++ test/module3_2/SquareSumRealizationTest.java
@@ -1,32 +1,15 @@
 package module3_2;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SquareSumRealizationTest {
-SquareSumRealization square = new SquareSumRealization();
+    SquareSumRealization square = new SquareSumRealization();
+
     @Test
     public void testGetSquareSumFirst() throws Exception {
-        int[] value = new int[10];
-
-        for (int i = 0; i < value.length; i++) {
-            value[i] = i;
-        }
-
-
-        System.out.println(square.getSquareSumFirst(value));
-    }
-
-    @Test
-    public void testGetSquareSumSecond() throws Exception {
-
-
-        int[] value = new int[10];
-
-        for (int i = 0; i < value.length; i++) {
-            value[i] = i;
-        }
-
-
-        System.out.println(square.getSquareSumSecond(value));
+    int[] value = {1,2,3,4,5,6,7,8,9,10};
+        Long res = square.getSquareSum(value, 4);
+        Assert.assertEquals(385, res, 0);
     }
 }
